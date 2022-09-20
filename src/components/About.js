a516f14998b6de667bfa43cd39b8cd60ff69d258
Justin Saborouh <rubikmanjustin@gmail.com>
2022-09-20 09:32:23 -0700

    commit tree 8647f6ef414752a3ea2638179788aa4961d18c26

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio ? <p>{props.bio}</p> : ""}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <h3>Links</h3>
+      <a href={props.links.github}>{props.links.github}</a>
+      <a href={props.links.linkedin}>{props.links.linkedin}</a>
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,12 @@ import user from "../data/user";
 console.log(user);
 
 function App() {
+
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About bio={user.bio} links={user.links}/>
     </div>
   );
 }
